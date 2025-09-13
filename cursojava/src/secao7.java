b3641204d9c0c5a3ea4f2289b5b7568c04ac94c1
gbsalermo <119875310+gbsalermo@users.noreply.github.com>
2025-09-12 23:00:20 -0300

    Seção 7 - Funções para string
    Exemplos das aulas da seção 7( até o momento)

---
+++ cursojava/src/secao7.java
@@ -0,0 +1,52 @@
+package cursojava;
+
+public interface secao7 {
+	public static void main (String[] args) {
+		
+		/*FUNÇÕES PARA FORMATAR STRINGS
+		
+		  String original = "abcde FGHIJ ABC abc DEFG";
+		  String s01 = original.toLowerCase(); // Todas as letras minusculas
+		  String s02 = original.toUpperCase(); // Todas as letras maiusculas
+		  String s03 = original.trim(); // Elimina espaços do inicio e do final
+		  String s04 = original.substring(2); // corta as duas primeiras palavras
+		  String s05 = original.substring(2, 9); //RECORTA DO DOIS ATÉ O NOVE, UM CORTE DENTRO DO INTERVALO
+		  String s06 = original.replace('a', 'x'); //Troca o a por x, funciona com mais letras
+		  int i = original.indexOf("bc"); // localiza primeira aparição
+		  int j = original.lastindexOf("bc"); // localiza ultima aparição
+		  
+		  
+		  
+		  
+		  System.out.println("Original:" + original);
+		  System.out.println("toLowerCase: -" + s01 + "-");
+		  System.out.println("toUpperCase: -" + s02 + "-");
+		  System.out.println("trim: -" + s03 + "-");
+		  System.out.println("substring: -" + s04 + "-");
+		  System.out.println("substring 2: -" + s05 + "-");
+		  System.out.println("replace: -" + s06 + "-");
+		  System.out.println("primeira aparição bc: -" + i + "-");
+		  System.out.println("ultima aparição bc: -" + j + "-");
+		  */
+		  
+		  /*Operação split: recebe um separador e gera um vetor baseado no separador escolhido
+		  String s = "potato apple lemon";
+		  
+		  String [] vect = s.split(" "); // declaração de vetor
+		  String word1 = vect[0]; // atribuição dos vetores na variavel
+		  String word2 = vect[1];
+		  String word3 = vect[2];
+		  
+		  System.out.println(word1);
+		  System.out.println(word2);
+		  System.out.println(word3)
+*/
+
+	
+	
+	
+	
+	
+	}
+
+}
