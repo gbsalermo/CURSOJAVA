caf3304b1b3d1424dcc5d3fe14b7ee5f711c9573
gbsalermo <119875310+gbsalermo@users.noreply.github.com>
2025-09-20 08:41:34 -0300

    Membros estaticos part2

---
+++ POO_MEBESTATICOS/bin/src/util/calculadora2.java
@@ -0,0 +1,12 @@
+package util;
+
+public class calculadora2 { // nessa versão todos são transformados em estaticos para poder ser calculado independente de objetio
+	public static final double PI = 3.14159;
+	
+	public static double circunferencia(double raio) {
+		return 2.0 * PI * raio;
+	}
+	public static double volume(double raio) {
+		return 4.0 * PI * raio * raio * raio / 3.0;
+	}
+}
