5db962b166966113895998709e5db3c8724e6da2
gbsalermo <119875310+gbsalermo@users.noreply.github.com>
2025-09-23 23:20:53 -0300

    ATUALIZAÇÃO DO POOEXEMPLO1 - PRODUTO
    NESSA BRANCH ATUALIZEI O CODIGO COM CONSTRUTORES, THIS E SOBRECARGAS, UTILIZADOS PARA EXEMPLO NO CURSO

--- cursojava_POO_ex1/src/application/Progama.java
+++ cursojava_POO_ex1/src/application/Progama.java
@@ -13,24 +13,38 @@ public interface Progama {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		//ATUALIZAÇÃO PARA AULA DE CONTRUSTORES
 		Locale.setDefault(Locale.US);
+		//Quando executamos o comando abaixo, instaciamos um produto com seus atributos "vazios"
+		//Produto produto = new Produto(); //OBJETO EXEMPLO
 		
-		Produto produto = new Produto(); //crio o Scanner dessa vez para os valores irem pra produto
+		/*abaixo eu printo como o construtor produto tem valores nullo
+		System.out.println(produto.name);
+		System.out.println(produto.price);
+		System.out.println(produto.quantity);
+		*/
+		//É POSSIVEL FAZER COM QUE SEJA OBRIGADO O OBJETO RECEBER DADOS
+		
+		
+		Produto p = new Produto();//Construtor padrão
+		
 		System.out.println("Dados do produto: ");
 		System.out.println("Nome: ");
-		produto.name = sc.nextLine(); //referecio a entrada para o atributo name na classe
+		String name = sc.nextLine(); //Peço o name
 		System.out.println("Preço: ");
-		produto.price = sc.nextDouble(); // peço o preço
-		System.out.println("Quantidade em Estoque: ");
-		produto.quantity = sc.nextInt(); //peço a quantidade
-		
+		double price = sc.nextDouble(); // peço o preço
+	/*	System.out.println("Quantidade em Estoque: ");
+		int quantity = sc.nextInt(); //peço a quantidade*/ //Coloquei suspenso para teste de sobrecarga
+		//Produto produto = new Produto(name, price, quantity); //Aqui instancio o produto já com o construtor e assim os valores sao atribuidos de automaticamente
+		Produto produto = new Produto(name, price); //chamo o construtor com dois argumentos apenas(TESTE DE SOBRECARGA)
 
 		//System.out.println(produto.name + "," + produto.price + "," + produto.quantity); // print exemplo
 		System.out.println();
 		System.out.println(produto); //teste do toString dentro da classe
 		System.out.println(); // pulando linha
 		System.out.println("Qual o numero de produtos para ser adicionado ao estoque: ");
-		int quantity = sc.nextInt(); // adiciono o valor digitado em quantity
+		int quantity = sc.nextInt(); //declaro o quantity como int para o teste de sobrecarga
+		//quantity = sc.nextInt(); // adiciono o valor digitado em quantity
 		produto.addProducts(quantity);//ponho esse valor no calculo da classe addproducts
 		System.out.println();
 		System.out.println("Atualização: " + produto);
