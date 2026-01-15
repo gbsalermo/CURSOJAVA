4174c3f1a2472f5b39cba2bd8187404fe7231ab2
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-01-15 01:04:11 -0300

    Default Interface e Interface Comparable

---
+++ Interface_Comparable/src/entities/Employee.java
@@ -0,0 +1,46 @@
+package entities;
+
+//Implemento a interface Comparable
+public class Employee implements Comparable<Employee>{
+
+	private String name;
+	private Double salary;
+	
+	
+	public Employee(String name, Double salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Double getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+
+	@Override
+	public int compareTo(Employee other) {
+		
+		return name.compareTo(other.getName());
+		//Se quisesse comparar salarios
+		//return salary.compareTo(other.getSalary());
+		//Se quiser in verter é só por o -. ex: -salaryou -name
+	}
+	
+	
+}
