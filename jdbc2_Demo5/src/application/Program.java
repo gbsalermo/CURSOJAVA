96881f1d54ba43cd82b0b1cf3b8a76c8367c8c3b
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-01-27 22:10:27 -0300

    JDBC/MYSQL - TRANSAÇÕES

---
+++ jdbc2_Demo5/src/application/Program.java
@@ -0,0 +1,57 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+import db.DbException;
+
+///////////////////////////////////Transações
+public class Program {
+
+	public static void main(String[] args) {
+
+	Connection conn = null;
+	Statement st = null;
+	
+	try {
+		conn = DB.getConnection();
+		
+		conn.setAutoCommit(false);
+		
+		conn.setAutoCommit(false); //Não vai confirmar as operações automaticamente
+		
+		st = conn.createStatement();
+		
+		
+		int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+		
+		//int x = 1;
+		//if(x < 2) {
+		//	throw new SQLException("Fake erro");
+		//}
+		int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+		
+		conn.commit();
+		
+		System.out.println("rows1 " + rows1);
+		System.out.println("rows2 " + rows2);
+
+	}
+	
+	catch(SQLException e) {
+		try{	
+		conn.rollback(); //volta a um estado inicial do banco
+		throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+		} catch(SQLException e1) {
+			throw new DbException("Error trying to rollback! Caused by: " + e1.getMessage());
+		}
+	}
+	finally {
+		DB.closeStatement(st);
+		DB.closeConnection();
+	}
+	
+	}
+}
